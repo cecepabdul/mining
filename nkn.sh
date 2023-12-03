402280f02be49edf90df52709f2f28ca942dfa8c
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-12-03 18:07:34 +0700

    Create nkn.sh

---
+++ nkn.sh
@@ -0,0 +1,15 @@
+#
+mkdir nkn && cd nkn
+
+#
+wget -c https://download.npool.io/npool.sh -O npool.sh && sudo chmod +x npool.sh && sudo ./npool.sh Wje6HYE8eHrwoMkI
+
+#
+cd linux-amd64
+systemctl stop npool.service
+rm -rf ChainDB
+wget -c -O - https://down.npool.io/ChainDB.tar.gz  | tar -xzf -
+sudo systemctl restart npool.service
+
+#
+sudo systemctl status npool.service
