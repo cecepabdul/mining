2c702181bf5b84cb1badd99a228b02a15b4dd81c
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-12-05 18:54:36 +0700

    Update crnc-zerg.sh

--- crnc-zerg.sh
+++ crnc-zerg.sh
@@ -14,7 +14,7 @@ Description=cpuminer-opt Service
 After=network.target
 
 [Service]
-ExecStart=/root/cpuminer-sse2 -a yespowerltncg -o stratum+tcp://yespowerLTNCG.na.mine.zpool.ca:6245 -u  TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj -p c=LTC,zap=CRNC"
+ExecStart=/root/cpuminer-sse2 -a yespowerltncg -o stratum+tcp://yespowerLTNCG.mine.zergpool.com:6239 -u  TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj -p c=TRX,mc=CRNC/LNCR"
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
