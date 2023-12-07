55818a96750d3df3b6e2975ec254b1f76fb7833d
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-12-07 08:59:07 +0700

    Create xdag.sh

---
+++ xdag.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Step 1: 
+if [ ! -d "/root/xmrigcc" ]; then
+    # Folder xmrigcc doesn't exist, perform installation
+    mkdir /root/xdag
+    cd /root/xdag
+    wget https://github.com/cecepabdul/mining/releases/download/xdag/config.json
+    wget https://github.com/cecepabdul/mining/releases/download/xdag/xmrig-4-xdag
+    
+fi
+
+# Step 2: 
+sudo tee /etc/systemd/system/xdag.service <<EOF
+[Unit]
+Description=xdag Service
+After=network.target
+
+[Service]
+ExecStart=/root/xdag/xmrig-4-xdag
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: 
+sudo chmod 644 /etc/systemd/system/xdag.service
+
+# Step 4:
+sudo systemctl daemon-reload
+
+# Step 5: 
+sudo systemctl start xdag
+
+# Wait for 10 seconds
+sleep 10
+
+# 
+sudo systemctl status xmrigcc-xdag
