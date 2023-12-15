683a864302efaae06eca07e5e73fa92edf6655e4
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-12-15 12:25:52 +0700

    Create mrr-ghostrider.sh

---
+++ mrr-ghostrider.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 
+if [ ! -f "/root/xmrig-6.7.0/xmrig" ]; then
+    # File xmrig tidak ada, lakukan download dan ekstraksi
+    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+    tar -xvf xmrig-6.7.0-linux-x64.tar.gz
+fi
+
+# 
+sudo tee /etc/systemd/system/mrr.service <<EOF
+[Unit]
+Description=XMRig Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && ./xmrig -k -a ghostrider --donate-level 1 -o us-east01.miningrigrentals.com:51586 -u cecepabdul67.281058 -p x"
+WorkingDirectory=/root/xmrig-6.7.0
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 
+sudo chmod 644 /etc/systemd/system/mrr.service
+
+# 
+sudo systemctl daemon-reload
+
+# 
+sudo systemctl start mrr
+
+# 
+sleep 10
+
+# 
+sudo systemctl status mrr
