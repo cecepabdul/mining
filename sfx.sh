31dfb15db45dc13533ba3076ed63b755201d1b46
abdul <34927384+cecepabdul@users.noreply.github.com>
2023-12-30 14:33:53 +0700

    Create sfx.sh

---
+++ sfx.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 
+if [ ! -f "/root/xmrig-6.7.0/xmrig" ]; then
+    # File xmrig tidak ada, lakukan download dan ekstraksi
+    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+    tar -xvf xmrig-6.7.0-linux-x64.tar.gz
+fi
+
+# 
+sudo tee /etc/systemd/system/sfx.service <<EOF
+[Unit]
+Description=XMRig Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && ./xmrig -k -a rx/sfx --donate-level 1 -o us.fastpool.xyz:3034 -u Safex61Jc9684fqa4ThEuuXntyTrXKCj9WnaGVgsCgFHMepkMUgRAt6DxGTok35kvU3UCjch2g17HVenvsDwxjdET1ciehJaJuW4L@c -p x"
+WorkingDirectory=/root/xmrig-6.7.0
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 
+sudo chmod 644 /etc/systemd/system/sfx.service
+
+# 
+sudo systemctl daemon-reload
+
+# 
+sudo systemctl start sfx
+
+# 
+sleep 10
+
+# 
+sudo systemctl status sfx
