e88c200ec9647509073b82dc84f7cd9affb4b652
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-01-07 22:34:01 +0700

    Create inf-zls.sh

---
+++ inf-zls.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 
+if [ ! -f "/root/xmrig-6.7.0/xmrig" ]; then
+    # File xmrig tidak ada, lakukan download dan ekstraksi
+    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+    tar -xvf xmrig-6.7.0-linux-x64.tar.gz
+fi
+
+# 
+sudo tee /etc/systemd/system/inf.service <<EOF
+[Unit]
+Description=XMRig Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && ./xmrig --donate-level 1 -a cn/zls -o radioactive.sytes.net:11124 -u solo:infi89MBWGDLVvDxrVy3AQbVtmg4cYwXAYktgnSJhTYi3xGDQFViwhb3ctfJeRgnCsf7MDzbYPi7VdDiYmg3Y17LSy9cpSb7zch -p @cloud"
+WorkingDirectory=/root/xmrig-6.7.0
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 
+sudo chmod 644 /etc/systemd/system/inf-zls.service
+
+# 
+sudo systemctl daemon-reload
+
+# 
+sudo systemctl start inf-zls
+
+# 
+sleep 10
+
+# 
+sudo systemctl status inf-zls
