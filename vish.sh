efc473e91207da76b05b3f8f9f9e2273f96c7f9c
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-01-13 08:32:37 +0700

    Update vish.sh

--- vish.sh
+++ vish.sh
@@ -14,7 +14,7 @@ Description=cpuminer-opt Service
 After=network.target
 
 [Service]
-ExecStart=/root/cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-na.rplant.xyz:7079 -u upULJfTjCe5t8rNywBVRYGgvypTUPmMprH.cloud -p webpassword=cecepabdul
+ExecStart=/root/cpuminer-sse2 -a yespower  -o stratum+tcp://stratum-na.rplant.xyz:7079 -u upULJfTjCe5t8rNywBVRYGgvypTUPmMprH.b -p webpassword=cecepabdul
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
