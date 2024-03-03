819af10650bc83d1cd7fd258dce4558ce7aa3620
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-03 07:49:51 +0700

    Update waifu.sh

--- waifu.sh
+++ waifu.sh
@@ -10,7 +10,7 @@ if [ ! -f "/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI" ]; then
 fi
 
 # Step 2: 
-sudo tee /etc/systemd/system/bitnet.service <<EOF
+sudo tee /etc/systemd/system/waifu.service <<EOF
 [Unit]
 Description=cpuminer-opt Service
 After=network.target
@@ -27,16 +27,16 @@ WantedBy=multi-user.target
 EOF
 
 # Step 3: 
-sudo chmod 644 /etc/systemd/system/bitnet.service
+sudo chmod 644 /etc/systemd/system/waifu.service
 
 # Step 4:
 sudo systemctl daemon-reload
 
 # Step 5: 
-sudo systemctl start bitnet
+sudo systemctl start waifu
 
 # Wait for 10 seconds
 sleep 10
 
 # 
-sudo systemctl status bitnet
+sudo systemctl status waifu
