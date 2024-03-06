0787435250940c6ddb8dc84705047bd7e5a47786
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-06 11:57:28 +0700

    Update bitnet.sh

--- bitnet.sh
+++ bitnet.sh
@@ -2,11 +2,11 @@
 
 # Step 1: 
 
-if [ ! -f "/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI" ]; then
+if [ ! -f "/root/cpuminer-opt-aurum/cpuminer" ]; then
     # File cpuminer doesn't exist, perform installation
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
-    tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
-    cd SRBMiner-Multi-2-4-7
+    sudo apt update -y && apt install libssl-dev libssl3 -y
+    git clone https://github.com/bitnet-io/cpuminer-opt-aurum.git
+    cd cpuminer-opt-aurum && bash build.sh
 fi
 
 # Step 2: 
@@ -16,7 +16,7 @@ Description=cpuminer-opt Service
 After=network.target
 
 [Service]
-ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI --algorithm aurum --pool stratum-na.rplant.xyz:7109 --wallet bit1qm29v8gdsmgh5yramd5ql6nj5mtz4gm48869my9.bare --password webpassword=cecepabdul --keepalive true
+ExecStart=/root/cpuminer-opt-aurum/cpuminer -a aurum -o stratum-na.rplant.xyz:7109 -u bit1qm29v8gdsmgh5yramd5ql6nj5mtz4gm48869my9.cloud -p webpassword=cecepabdul
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
