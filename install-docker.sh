0c5df5900a36e1a635e1b358e79905b8a449572f
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-11 12:53:25 +0700

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-sudo su -
-sudo apt update
-
-if ! command -v docker &> /dev/null
-then
-    echo "Docker belum terinstal. Melakukan instalasi..."
+#
+if ! command -v docker &> /dev/null; then
+    echo "Docker belum terinstal, menginstal Docker..."
+    # Install Docker
+    apt update -y
     curl -fsSL https://get.docker.com | sh
+    echo "Docker telah terinstal."
 else
     echo "Docker sudah terinstal."
 fi
+
+# 
+if docker ps -a --format '{{.Names}}' | grep -q 'tm'; then
+    echo "Menghapus container Docker dengan nama 'tm'..."
+    docker stop tm
+    docker rm tm
+fi
+
+# 
+docker pull traffmonetizer/cli_v2:latest
+docker run -d --name tm traffmonetizer/cli_v2:latest start accept --token j/8mvzZq1NGzVUwAjjAZYmPYHlHyLchRNyK1BNKNL6E= --device-name cloud
