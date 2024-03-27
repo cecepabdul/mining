20ceeae6ef2d77d6bd22cb1b92d736617e079c24
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-27 20:10:06 +0700

    Create update-qubic.sh

---
+++ update-qubic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+rm -rf rqiner-x86
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v0.3.15/rqiner-x86
+chmod +x rqiner-x86
+
+#
+total_cpu=$(nproc)
+
+#
+sudo tee /etc/systemd/system/qubic.service <<EOF
+[Unit]
+Description=cpuminer-opt Service
+After=network.target
+
+[Service]
+ExecStart=/root/rqiner-x86-haswell -t $total_cpu -i VHTDSWYLKHBYCAFESSZGSHABLOEDXZDQYYQZJXNXXAKHDDUJXQZFXQHCHONE
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+
+systemctl daemon reload
+systemctl stop qubic
+systemctl start qubic
+
+sleep 5
+
+journalctl -f -u qubic
+
