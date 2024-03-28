aa5d84cb68067332934a0046c865cd5a4a230d52
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-29 02:09:47 +0700

    Update qubic-portal.sh

--- qubic-portal.sh
+++ qubic-portal.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 #
-wget wget -O qli-Service-install.sh https://dl.qubic.li/cloud-init/qli-Service-install.sh
+wget -O qli-Service-install.sh https://dl.qubic.li/cloud-init/qli-Service-install.sh
 
 total_cpu=$(nproc)
 
 #
 bash qli-Service-install.sh $total_cpu eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImFjZGRlNzEzLWE3MTMtNGQwYy05MTY0LTFlOTQ0M2I1NDU0MyIsIk1pbmluZyI6IiIsIm5iZiI6MTcxMTU2MzQxOSwiZXhwIjoxNzQzMDk5NDE5LCJpYXQiOjE3MTE1NjM0MTksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.VB3Vt7KbqhBmkA-0A3wEAdRRKS4fgI9Fczggag4Qhwf9WF5Jx5MRfrLu3VBNQH--hrzyln88TUaxgfyPzwnlOg
 
+sleep 10
 #
 tail -f /var/log/qli.log
