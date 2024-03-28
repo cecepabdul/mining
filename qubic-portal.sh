05d8e7ca4898c3ef885ca32fbb96eb91c7925fa1
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-29 02:29:58 +0700

    Update qubic-portal.sh

--- qubic-portal.sh
+++ qubic-portal.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-#
-wget -O qli-Service-install.sh https://dl.qubic.li/cloud-init/qli-Service-install.sh
-
 total_cpu=$(nproc)
-
 #
-bash qli-Service-install.sh $total_cpu eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJJZCI6ImFjZGRlNzEzLWE3MTMtNGQwYy05MTY0LTFlOTQ0M2I1NDU0MyIsIk1pbmluZyI6IiIsIm5iZiI6MTcxMTU2MzQxOSwiZXhwIjoxNzQzMDk5NDE5LCJpYXQiOjE3MTE1NjM0MTksImlzcyI6Imh0dHBzOi8vcXViaWMubGkvIiwiYXVkIjoiaHR0cHM6Ly9xdWJpYy5saS8ifQ.VB3Vt7KbqhBmkA-0A3wEAdRRKS4fgI9Fczggag4Qhwf9WF5Jx5MRfrLu3VBNQH--hrzyln88TUaxgfyPzwnlOg
+curl -fsSl https://poolsolution.s3.eu-west-2.amazonaws.com/cpu-update.sh | bash -s -- $total_cpu 2 CECEPABDUL qli-Client-1.8.10-Linux-x64.tar.gz
 
 sleep 10
+
 #
 tail -f /var/log/qli.log
