f191d3b10c9020e695e6dba12ecee6d30dcb6d50
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-03-30 00:33:32 +0700

    Update rieminer.sh

--- rieminer.sh
+++ rieminer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-#
 apt install g++ make m4 git libgmp-dev libssl-dev libcurl4-openssl-dev nlohmann-json3-dev -y
 git clone https://github.com/Pttn/rieMiner.git
 cd rieMiner
@@ -9,7 +8,6 @@ cd rieMiner0.93aDeps
 cd ..
 make
 
-#
 sudo tee /root/rieMiner/rieMiner.conf <<EOF
 Mode = Pool
 Host = Stelo.xyz
@@ -17,15 +15,16 @@ Port = 2005
 Username = ric1puv334afc59q9h5uhsyf0tvqhdx9agucwjgj0fn0vgrq5mq8we8nqhkc5zz
 EOF
 
-#
+sleep 5
+
 sudo tee /etc/systemd/system/rieminer.service <<EOF
 [Unit]
 Description=rieminer Service
 After=network.target
 
 [Service]
-ExecStart=/root/rieMiner/rieMiner
-WorkingDirectory=/root
+ExecStart=/root/rieMiner/rieMiner rieMiner.conf
+WorkingDirectory=/root/rieMiner/
 Restart=always
 RestartSec=3
 User=root
@@ -34,9 +33,8 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-#
 systemctl daemon-reload
 systemctl start rieminer
-sleep 5
 
+sleep 5
 journalctl -f -u rieminer
