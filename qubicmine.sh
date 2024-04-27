77763fb406aab0d8c2d49cb77937d9eaece69b14
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-04-27 11:20:08 +0700

    Create qubicmine.sh

---
+++ qubicmine.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#
+mkdir ~/qubicmine;
+cd ~/qubic;
+wget https://github.com/cecepabdul/mining/releases/download/xdag/libclient;
+wget https://github.com/cecepabdul/mining/releases/download/xdag/qubic-pool-miner;
+chmod +x qubic-pool-miner;
+
+#-----------
+total_cpu=$(grep -c "^processor" /proc/cpuinfo)
+hugepages=$((total_cpu * 55))
+sudo /usr/sbin/sysctl -w vm.nr_hugepages=$hugepages
+
+#
+sudo tee /root/qubicmine/qubicmine.json <<EOF
+
+{
+    "Settings": {
+        "amountOfThreads": $total_cpu,
+        "payoutId": "VHTDSWYLKHBYCAFESSZGSHABLOEDXZDQYYQZJXNXXAKHDDUJXQZFXQHCHONE",
+        "alias": "cloud"
+    }
+}
+
+EOF
+
+#-------
+
+screen -S qubicmine /root/qubicmine/qubic-pool-miner
