d28dbd44485a0c3d1e20f0faf05f2f4a1394ca31
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-05-05 08:19:51 +0700

    Update qubicmine.sh

--- qubicmine.sh
+++ qubicmine.sh
@@ -19,7 +19,7 @@ sudo tee /root/qubicmine/qubicmine.json <<EOF
     "Settings": {
         "amountOfThreads": $total_cpu,
         "payoutId": "VHTDSWYLKHBYCAFESSZGSHABLOEDXZDQYYQZJXNXXAKHDDUJXQZFXQHCHONE",
-        "alias": "cloud-8"
+        "alias": "cloud"
     }
 }
 
