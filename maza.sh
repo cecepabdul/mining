765ccb044704b9abf9957c3148984d20052021e0
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-05-25 07:48:56 +0700

    Update maza.sh

--- maza.sh
+++ maza.sh
@@ -14,7 +14,7 @@ Description=SRBMiner-MULTI Service
 After=network.target
 
 [Service]
-ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://us.fastpool.xyz:6001 --wallet solo:MLegX9RugBiRMpJjmENLZEtnoPtGrF7o1h@cloud --password x
+ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://us.fastpool.xyz:6001 --wallet solo:MLegX9RugBiRMpJjmENLZEtnoPtGrF7o1h@b --password x
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
