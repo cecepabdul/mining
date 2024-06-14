81bdff9c0f0838fc72fe23d1389d0f7eea73872c
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-06-14 21:51:54 +0700

    Update pingpong.sh

--- pingpong.sh
+++ pingpong.sh
@@ -1,75 +1,35 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <key>"
-    exit 1
+# Memeriksa apakah parameter device_id diberikan
+if [ -z "$1" ]; then
+  echo "Usage: $0 device_id"
+  exit 1
 fi
 
-key=$1
+DEVICE_ID=$1
 
-# Check if Docker is installed
-if ! command -v docker &> /dev/null; then
-    echo "Docker is not installed, installing..."
-    # Install Docker
-    if ! curl -fsSL https://get.docker.com | sh; then
-        echo "Failed to install Docker"
-        exit 1
-    fi
-fi
-
-# Check if PINGPONG file exists
-if [ ! -f "/root/PINGPONG" ]; then
-    # File PINGPONG doesn't exist, perform installation
-    echo "Downloading PINGPONG..."
-    if ! wget https://github.com/cecepabdul/mining/releases/download/xdag/PINGPONG; then
-        echo "Failed to download PINGPONG"
-        exit 1
-    fi
-    chmod +x PINGPONG
-fi
-
-# Continue with systemd service configuration
-
-# Step 2: 
-if ! sudo tee /etc/systemd/system/pingpong.service <<EOF; then
+# Buat unit file systemd pingpong.service
+cat <<EOL > /etc/systemd/system/pingpong.service
 [Unit]
-Description=pingpong
+Description=PingPong Service
 After=network.target
 
 [Service]
-ExecStart=/root/PINGPONG --key $key
-WorkingDirectory=/root
-Restart=always
-RestartSec=3
-User=root
+Type=simple
+ExecStart=/bin/bash -c 'curl -O https://pingpong-build.s3.ap-southeast-1.amazonaws.com/linux/latest/PINGPONG && chmod +x ./PINGPONG && ./PINGPONG --key $DEVICE_ID'
+Restart=on-failure
 
 [Install]
 WantedBy=multi-user.target
-EOF
-    echo "Failed to create pingpong.service"
-    exit 1
-fi
+EOL
 
-# Step 3: 
-if ! sudo chmod 644 /etc/systemd/system/pingpong.service; then
-    echo "Failed to set permissions for pingpong.service"
-    exit 1
-fi
+# Reload systemd untuk memuat unit file baru
+systemctl daemon-reload
 
-# Step 4:
-if ! sudo systemctl daemon-reload; then
-    echo "Failed to reload systemd daemon"
-    exit 1
-fi
+# Mengaktifkan service untuk dijalankan saat boot
+systemctl enable pingpong.service
 
-# Step 5: 
-if ! sudo systemctl start pingpong; then
-    echo "Failed to start pingpong service"
-    exit 1
-fi
+# Menjalankan service
+systemctl start pingpong.service
 
-# Wait for 10 seconds
-sleep 10
-
-# Check status
-sudo systemctl status pingpong
+echo "PingPong service has been set up and started."
