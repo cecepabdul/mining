1e24c2e86ca96e344ee0e9ee8313fd499f4ba3e0
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-06-18 12:29:40 +0700

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Step 1: 
-if [ ! -f "/root/cpuminer-sse2" ]; then
-    # File cpuminer-avx doesn't exist, perform installation
-    wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.24/cpuminer-opt-linux.tar.gz -O /root/cpuminer-opt-linux.tar.gz
-    tar -xvf /root/cpuminer-opt-linux.tar.gz -C /root
+if [ ! -f "/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI" ]; then
+    # File cpuminer doesn't exist, perform installation
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
+    tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
+    cd SRBMiner-Multi-2-4-7
 fi
 
-# Step 2: 
+# Step 2: Create systemd configuration file srb.service
 sudo tee /etc/systemd/system/sugar.service <<EOF
 [Unit]
-Description=cpuminer-opt Service
+Description=SRBMiner-MULTI Service
 After=network.target
 
 [Service]
-ExecStart=/root/./cpuminer-sse2 -a yespowersugar -o stratum+tcp://stratum-na.rplant.xyz:7042 -u sugar1q4vcxkt82achnuqzwsavgha6jyurtgywj26grpj -p webpassword=cecepabdul
+ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://yespowerSUGAR.na.mine.zpool.ca:6241 --wallet LYNzmqsz1rnfCkLiQisBHDkpWJE5YupiYd --password c=LTC
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
@@ -24,17 +24,17 @@ User=root
 WantedBy=multi-user.target
 EOF
 
-# Step 3: 
+# Step 3: Set permissions on the configuration file
 sudo chmod 644 /etc/systemd/system/sugar.service
 
-# Step 4:
+# Step 4: Reload systemd configuration
 sudo systemctl daemon-reload
 
-# Step 5: 
-sudo systemctl start sugar
+# Step 5: Start the srb service
+sudo systemctl start sugar.service
 
 # Wait for 10 seconds
 sleep 10
 
-# 
-sudo systemctl status sugar
+# Check the status of the srb service
+journalctl -f -u sugar.service
