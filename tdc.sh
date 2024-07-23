b5b1ceec57115948e4db50e00b5400a80e7b295d
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-07-23 11:14:54 +0700

    Update tdc.sh

--- tdc.sh
+++ tdc.sh
@@ -14,7 +14,7 @@ Description=cpuminer-opt Service
 After=network.target
 
 [Service]
-ExecStart=/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI -a yespowertide  -o stratum+tcp://stratum-na.rplant.xyz:7059 -u TYj2cUrnoX7snkiTnpWHRKYCRZNAczZXFR.b -p webpassword=cecepabdul
+ExecStart=/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI -a yespowertide  -o stratum+tcp://stratum-na.rplant.xyz:7059 -u TYj2cUrnoX7snkiTnpWHRKYCRZNAczZXFR.c -p webpassword=cecepabdul
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
