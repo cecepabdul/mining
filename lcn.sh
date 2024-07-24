120c64e76c960bf83f386b09f8393c4be13c1ab2
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-07-24 10:14:12 +0700

    Create lcn.sh

---
+++ lcn.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ ! -f "/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI" ]; then
+    # File cpuminer doesn't exist, perform installation
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.9/SRBMiner-Multi-2-5-9-Linux.tar.gz
+    tar -xvf SRBMiner-Multi-2-5-9-Linux.tar.gz
+    cd SRBMiner-Multi-2-5-9
+fi
+
+# Step 2: Create systemd configuration file srb.service
+sudo tee /etc/systemd/system/lcn.service <<EOF
+[Unit]
+Description=SRBMiner-MULTI Service
+After=network.target
+
+[Service]
+ExecStart=/root/SRBMiner-Multi-2-5-9/SRBMiner-MULTI -a flex -o stratum+ssl://eu.mpool.live:5213 -u LCN=lc1qs7v5en4x7v2azktrmqqr0kyff2y6vzxfnuwl5q -p m=solo
+
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Set permissions on the configuration file
+sudo chmod 644 /etc/systemd/system/lcn.service
+
+# Step 4: Reload systemd configuration
+sudo systemctl daemon-reload
+
+# Step 5: Start the srb service
+sudo systemctl start lcn.service
+
+# Wait for 10 seconds
+sleep 10
+
+# Check the status of the srb service
+journalctl -f -u lcn.service
