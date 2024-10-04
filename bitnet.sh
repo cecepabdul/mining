62a925d1c33cb4909040b8173b36dd8b39549564
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-10-04 21:42:43 +0700

    Update bitnet.sh

--- bitnet.sh
+++ bitnet.sh
@@ -2,11 +2,11 @@
 
 # Step 1: 
 
-if [ ! -f "/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI" ]; then
+if [ ! -f "/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI" ]; then
     # File cpuminer doesn't exist, perform installation
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
-    tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
-    cd SRBMiner-Multi-2-4-7
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.6/SRBMiner-Multi-2-6-6-Linux.tar.gz
+    tar -xvf SRBMiner-Multi-2-6-6-Linux.tar.xz
+    cd SRBMiner-Multi-2-6-6
 fi
 
 # Step 2: 
@@ -16,7 +16,7 @@ Description=cpuminer-opt Service
 After=network.target
 
 [Service]
-ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI -a aurum -o stratum-na.rplant.xyz:7109 -u bit1qm29v8gdsmgh5yramd5ql6nj5mtz4gm48869my9.cloud -p webpassword=cecepabdul
+ExecStart=/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI -a aurum -o asia.cpumining.xyz:5177 -u bit1qm29v8gdsmgh5yramd5ql6nj5mtz4gm48869my9.cloud -p webpassword=cecepabdul
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
@@ -39,4 +39,4 @@ sudo systemctl start bitnet
 sleep 10
 
 # 
-sudo systemctl status bitnet
+journalctl -f -u bitnet
