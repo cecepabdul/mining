193d28d193cf6a04f5586ce5ee7c2b7060e6eeab
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-10-12 07:46:43 +0700

    Create ore-logs.sh

---
+++ ore-logs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Menampilkan log dari Docker yang berisi informasi "Pool Submitted Difficulty", "Pool Earned", "Miner Submitted Difficulty", dan "Miner Earned"
+docker logs -f hq-client 2>&1 | grep -E "Pool Submitted Difficulty|Pool Earned|Miner Submitted Difficulty|Miner Earned" | while read -r line; do
+  echo "$line"
+  
+  # Jika baris berisi "Miner Earned", tambahkan pemisah untuk setiap set informasi
+  if [[ "$line" == *"Miner Earned"* ]]; then
+    echo "----------------------"
+    echo ""
+  fi
+done
