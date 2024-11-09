ff820cae80f4d890d23502c05701863f7d4830bf
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-11-09 19:40:38 +0700

    Update idle.sh

--- idle.sh
+++ idle.sh
@@ -4,4 +4,4 @@
 total_cpu=$(nproc)
 
 # Jalankan SRBMiner-MULTI dengan parameter yang Anda inginkan
-/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI -a power2b -o us-east01.miningrigrentals.com:3333 -u cecepabdul67.325911 -p x -t $total_cpu
+/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI -a power2b -o us-east01.miningrigrentals.com:3333 -u cecepabdul67.325911 -p x
