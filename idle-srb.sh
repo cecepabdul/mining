6a24f4a5bfa2ce72ca1d4db55f50116be82e8443
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-05 11:52:25 +0700

    Update idle-srb.sh

--- idle-srb.sh
+++ idle-srb.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.6/SRBMiner-Multi-2-6-6-Linux.tar.gz
+tar -xvf SRBMiner-Multi-2-6-6-Linux.tar.gz
+
 # Mendapatkan jumlah total CPU di sistem
 total_cpu=$(nproc)
 
+chmod +x ./SRBMiner-Multi-2-6-6/SRBMiner-MULTI
+
 # Jalankan SRBMiner-MULTI dengan parameter yang Anda inginkan
-/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI -a power2b -o us-east01.miningrigrentals.com:3333 -u cecepabdul67.325911 -p x
+./SRBMiner-Multi-2-6-6/SRBMiner-MULTI -a power2b -o stratum+tcp://stratum-mining-pool.zapto.org:3765 -u MpTmkKueJSaKe9TJDbefvJooSLENUSkgpQ.b -p x -t $total_cpu
