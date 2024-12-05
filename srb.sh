acfa74a8a7bb20a6086db4a47201242c98b4cdaa
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-05 11:25:07 +0700

    Update srb.sh

--- srb.sh
+++ srb.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Nama atau PID dari proses qubic
-QUBIC_NAME="cpu_avx2_traine"  # ganti dengan nama atau PID aplikasi Qubic
+QUBIC_NAME="rqiner"  # ganti dengan nama atau PID aplikasi Qubic
 CPU_THRESHOLD=10  # Batas persentase CPU untuk idle (misalnya 10%)
 CHECK_INTERVAL=60  # Interval waktu pengecekan dalam detik
 
@@ -12,7 +12,7 @@ while true; do
     if [ -z "$CPU_USAGE" ]; then
         echo "Proses Qubic tidak ditemukan."
         # Jika Qubic tidak berjalan, pastikan SRB berjalan
-        pgrep -f SRBMiner-MULTI > /dev/null || /root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI  --disable-gpu -a power2b -o us-east01.miningrigrentals.com:3333 -u cecepabdul67.325911 -p x &
+        pgrep -f SRBMiner-MULTI > /dev/null || /root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI --disable-gpu -a power2b -o stratum+tcp://stratum-mining-pool.zapto.org:3765 -u mbc1q4978y635chz7rk8uhwd9sm9vmzq3z657wmrd5w -p x &
     elif (( $(echo "$CPU_USAGE > $CPU_THRESHOLD" | bc -l) )); then
         echo "Qubic sedang aktif (CPU: $CPU_USAGE%), menghentikan SRB."
         # Hentikan SRB jika berjalan
@@ -20,7 +20,7 @@ while true; do
     else
         echo "Qubic sedang idle (CPU: $CPU_USAGE%), menjalankan SRB."
         # Jika Qubic idle, jalankan SRB jika belum berjalan
-        pgrep -f SRBMiner-MULTI > /dev/null || /root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI  --disable-gpu -a power2b -o us-east01.miningrigrentals.com:3333 -u cecepabdul67.325911 -p x &
+        pgrep -f SRBMiner-MULTI > /dev/null || /root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI --disable-gpu -a power2b -o stratum+tcp://stratum-mining-pool.zapto.org:3765 -u mbc1q4978y635chz7rk8uhwd9sm9vmzq3z657wmrd5w -p x &
     fi
 
     # Tunggu sebelum pengecekan berikutnya
