672a1c7b1f47169f87a10b0a9089cfd56ca64d16
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-08 09:11:40 +0700

    Update sugar.sh

--- sugar.sh
+++ sugar.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 
-if [ ! -f "/root/cpu/cpuminer-sse2" ]; then
-    # File cpuminer-avx doesn't exist, perform installation
-    mkdir cpu
-    cd cpu
-    wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.40/cpuminer-opt-linux-5.0.40.tar.gz -O /root/cpu/cpuminer-opt-linux.tar.gz
-    tar -xvf /root/cpu/cpuminer-opt-linux.tar.gz -C /root/cpu
+if [ ! -f "/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI" ]; then
+    # File cpuminer doesn't exist, perform installation
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
+    tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
+    cd SRBMiner-Multi-2-4-7
 fi
 
 # Step 2: Create systemd configuration file srb.service
@@ -16,7 +15,7 @@ Description=SRBMiner-MULTI Service
 After=network.target
 
 [Service]
-ExecStart=/root/cpu/cpuminer-sse2 -a yespowersugar -o stratum+tcp://yespowerSUGAR.mine.zergpool.com:6535 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj -p c=TRX,mc=SUGAR
+ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI -a yespowersugar -o stratum+tcp://yespowerSUGAR.mine.zergpool.com:6535 -u TZGQwQ58mdfVg5Tr7ap91pDq4GGARtGYrj -p c=TRX,mc=SUGAR -t $(nproc)
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
@@ -32,8 +31,3 @@ sudo chmod 644 /etc/systemd/system/sugar.service
 # Step 4: Reload systemd configuration
 sudo systemctl daemon-reload
 
-# Step 5: Start the srb service
-sudo systemctl start sugar.service
-
-# Wait for 10 seconds
-sleep 10
