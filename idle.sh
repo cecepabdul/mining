7aac5a63eef009dfbd96b96b337dec1a721b7885
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-08 12:01:36 +0700

    Update idle.sh

--- idle.sh
+++ idle.sh
@@ -3,7 +3,7 @@
 QUBIC_NAME="rqiner"  # Nama proses
 CPU_THRESHOLD=10     # Batas penggunaan CPU untuk idle
 CHECK_INTERVAL=60    # Interval pengecekan dalam detik
-SYSTEMD_NAME="sugar"  # Nama layanan yang disesuaikan
+SYSTEMD_NAME="maza"  # Nama layanan yang disesuaikan
 
 # Fungsi untuk menghentikan pemantauan jika sudah berjalan
 stop_monitoring() {
