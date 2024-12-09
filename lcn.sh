ddba6a6e76fff48e4ef5875e5ad2a65c7afc61a7
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-09 09:21:55 +0700

    Update lcn.sh

--- lcn.sh
+++ lcn.sh
@@ -33,9 +33,3 @@ sudo systemctl daemon-reload
 
 # Step 5: Start the srb service
 sudo systemctl start lcn.service
-
-# Wait for 10 seconds
-sleep 10
-
-# Check the status of the srb service
-journalctl -f -u lcn.service
