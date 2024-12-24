1991840b9a83196ebd7dd2f7b42b8a045cf100f3
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-24 21:56:57 +0700

    Create eti.sh

---
+++ eti.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 
+if [ ! -f "/root/xmrig-6.7.0/xmrig" ]; then
+    # File xmrig tidak ada, lakukan download dan ekstraksi
+    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+    tar -xvf xmrig-6.7.0-linux-x64.tar.gz
+fi
+
+# 
+sudo tee /etc/systemd/system/eti.service <<EOF
+[Unit]
+Description=XMRig Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && ./xmrig -a rx/0 -o us.0xpool.io:3333 -u 0xED792bA59D9547436a48c00D0528424E1473B69A -p x"
+WorkingDirectory=/root/xmrig-6.7.0
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 
+sudo chmod 644 /etc/systemd/system/eti.service
+
+# 
+sudo systemctl daemon-reload
+
+# 
+sudo systemctl start eti.service
+
+# 
+sleep 10
+
+# 
+sudo systemctl status eti.service
