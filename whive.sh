7b3fe70ee2190ba69758004ff4b27b7e7d9a4898
abdul <34927384+cecepabdul@users.noreply.github.com>
2024-12-31 18:55:06 +0700

    Create whive.sh

---
+++ whive.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [ ! -f "/root/SRBMiner-Multi-2-7-4/SRBMiner-MULTI" ]; then
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.7.4/SRBMiner-Multi-2-7-4-Linux.tar.gz
+    tar -xvf SRBMiner-Multi-2-7-4-Linux.tar.gz
+    cd SRBMiner-Multi-2-7-4
+fi
+
+# Step 2: Create systemd configuration file srb.service
+sudo tee /etc/systemd/system/whive.service <<EOF
+[Unit]
+Description=SRBMiner-MULTI Service
+After=network.target
+
+[Service]
+ExecStart=/root/SRBMiner-Multi-2-7-4/SRBMiner-MULTI -a yespower -o stratum+tcp://stratum.aikapool.com:3933 -u cecepabdul.cloud -p x
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Set permissions on the configuration file
+sudo chmod 644 /etc/systemd/system/whive.service
+
+# Step 4: Reload systemd configuration
+sudo systemctl daemon-reload
+
+# Step 5: Start the srb service
+sudo systemctl start whive.service
+
+# Wait for 10 seconds
+sleep 10
+
+# Check the status of the srb service
+journalctl -fu whive.service
