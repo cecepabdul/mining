0960cb3278df25d9cb5906898874b35bef9de345
abdul <34927384+cecepabdul@users.noreply.github.com>
2025-01-04 23:50:01 +0700

    Update dogemone.sh

--- dogemone.sh
+++ dogemone.sh
@@ -21,7 +21,7 @@ Description=SBP Miner Service
 After=network.target
 
 [Service]
-ExecStart=/root/dme/sbp --donate-level 1 -o dme.us.ekapool.com:13666 -u solo:dmeUyLioqdQ8L2iVGfa9CiPkv86PJjhPQ1gywrnsRSvbWFdptYzTsjWSdmPuyVf9ijC91nHYmA31kesQ1ozZShBj3EdukbSwak -p x -t $CPU_THREADS -v 3
+ExecStart=/root/dme/sbp dme.us.ekapool.com:13666 -u solo:dmeUyLioqdQ8L2iVGfa9CiPkv86PJjhPQ1gywrnsRSvbWFdptYzTsjWSdmPuyVf9ijC91nHYmA31kesQ1ozZShBj3EdukbSwak -p x -t $CPU_THREADS -v 3
 WorkingDirectory=/root/dme
 Restart=always
 RestartSec=3
