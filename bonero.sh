fcf67b3abe95e5e0725066ad713eafd2961df6f7
abdul <34927384+cecepabdul@users.noreply.github.com>
2025-02-28 10:54:42 +0700

    Create bonero.sh

---
+++ bonero.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# 
+if [ ! -f "/root/xmrig-6.7.0/xmrig" ]; then
+    # File xmrig tidak ada, lakukan download dan ekstraksi
+    wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+    tar -xvf xmrig-6.7.0-linux-x64.tar.gz
+fi
+
+# 
+sudo tee /etc/systemd/system/bonero.service <<EOF
+[Unit]
+Description=XMRig Service
+After=network.target
+
+[Service]
+ExecStart=/bin/bash -c "cd /root/xmrig-6.7.0 && ./xmrig -a randomx --url randomx.rplant.xyz:7139 --user SEXTmoNF3T8VPZ5Y5iEuUS4jEiBVDAisLW1bzcj2t4dKRLmqBoc7hno4jrp8B2n7BUbSbYquJzuZzGoVey3jCJUu5TzC9zSs75.c"
+WorkingDirectory=/root/xmrig-6.7.0
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 
+sudo chmod 644 /etc/systemd/system/bonero.service
+
+# 
+sudo systemctl daemon-reload
+
+# 
+sudo systemctl start bonero.service
+
+# 
+sleep 10
+
+# 
+journalctl -fu bonero.service
