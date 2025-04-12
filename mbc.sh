95ac495a77bcc0a546352063228eb52c8fa92583
abdul <34927384+cecepabdul@users.noreply.github.com>
2025-04-12 13:54:48 +0700

    Create mbc.sh

---
+++ mbc.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Step 1: 
+
+if [ ! -f "/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI" ]; then
+    # File cpuminer doesn't exist, perform installation
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.6.6/SRBMiner-Multi-2-6-6-Linux.tar.gz
+    tar -xvf SRBMiner-Multi-2-6-6-Linux.tar.gz
+    cd SRBMiner-Multi-2-6-6
+fi
+
+total_cpu=$(grep -c "^processor" /proc/cpuinfo)
+
+# Step 2: Create systemd configuration file srb.service
+sudo tee /etc/systemd/system/mbc.service <<EOF
+[Unit]
+Description=SRBMiner-MULTI Service
+After=network.target
+
+[Service]
+ExecStart=/root/SRBMiner-Multi-2-6-6/SRBMiner-MULTI -a power2b -o stratum+tcp://stratum-na.rplant.xyz:7022 -u MpTmkKueJSaKe9TJDbefvJooSLENUSkgpQ -p x -t $total_cpu
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: Set permissions on the configuration file
+sudo chmod 644 /etc/systemd/system/mbc.service
+
+# Step 4: Reload systemd configuration
+sudo systemctl daemon-reload
+
+# Step 5: Start the srb service
+sudo systemctl start mbc.service
+
+journalctl -fu mbc.service
