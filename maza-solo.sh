65fb8ac5ea9169dc110cca3be21f845a1e75e10c
abdul <34927384+cecepabdul@users.noreply.github.com>
2025-05-03 02:40:53 +0700

    Update and rename maza-aika.sh to maza-solo.sh

--- maza-solo.sh
+++ maza-solo.sh
@@ -2,19 +2,19 @@
 
 if [ ! -f "/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI" ]; then
     # File cpuminer doesn't exist, perform installation
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.4.7/SRBMiner-Multi-2-4-7-Linux.tar.xz
-    tar -xvf SRBMiner-Multi-2-4-7-Linux.tar.xz
-    cd SRBMiner-Multi-2-4-7
+    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.5.9/SRBMiner-Multi-2-5-9-Linux.tar.gz
+    tar -xvf SRBMiner-Multi-2-5-9-Linux.tar.gz
+    cd SRBMiner-Multi-2-5-9
 fi
 
 # Step 2: Create systemd configuration file srb.service
-sudo tee /etc/systemd/system/maza-aika.service <<EOF
+sudo tee /etc/systemd/system/maza-solo.service <<EOF
 [Unit]
 Description=SRBMiner-MULTI Service
 After=network.target
 
 [Service]
-ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum.aikapool.com:3904 --wallet abdul67.bare --password x
+ExecStart=/root/SRBMiner-Multi-2-4-7/SRBMiner-MULTI --disable-gpu --algorithm minotaurx --pool stratum+tcp://162.253.42.21:3062 --wallet MSwVfQxBUWE2sSm19DV7jTivTTqRwHXnts --password x
 WorkingDirectory=/root
 Restart=always
 RestartSec=3
@@ -25,16 +25,16 @@ WantedBy=multi-user.target
 EOF
 
 # Step 3: Set permissions on the configuration file
-sudo chmod 644 /etc/systemd/system/maza-aika.service
+sudo chmod 644 /etc/systemd/system/maza-solo.service
 
 # Step 4: Reload systemd configuration
 sudo systemctl daemon-reload
 
 # Step 5: Start the srb service
-sudo systemctl start maza-aika
+sudo systemctl start maza-solo
 
 # Wait for 10 seconds
 sleep 10
 
 # Check the status of the srb service
-journalctl -f -u maza-aika
+journalctl -f -u maza-solo
