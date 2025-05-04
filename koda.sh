ec5dbbac6aff32ebf9f3e591cfee84250629e481
abdul <34927384+cecepabdul@users.noreply.github.com>
2025-05-05 01:37:22 +0700

    Create koda.sh

---
+++ koda.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+wget https://github.com/kobradag/kobrad/releases/download/v1.0.3/kobrad-linux.zip && unzip kobrad-linux.zip && rm kobrad-linux.zip
+
+
+# Step 2: 
+sudo tee /etc/systemd/system/koda.service <<EOF
+[Unit]
+Description=kobrad
+After=network.target
+
+[Service]
+ExecStart=/root/kobrad-linux/kobrad --utxoindex
+WorkingDirectory=/root
+Restart=always
+RestartSec=3
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Step 3: 
+sudo chmod 644 /etc/systemd/system/koda.service
+
+# Step 4:
+sudo systemctl daemon-reload
+
+# Step 5: 
+sudo systemctl start koda.service
+
+# Wait for 10 seconds
+sleep 10
+
+# 
+journalctl -fu koda.service
